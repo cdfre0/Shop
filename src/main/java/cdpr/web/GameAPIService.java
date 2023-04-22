ed1438d6d4678d13b1080bd260060a861b61d3bd
Jan Mich <jaanmiich@gmail.com>
2023-04-23 00:05:55 +0800

    Added update Quantity and Delete

--- src/main/java/cdpr/web/GameAPIService.java
+++ src/main/java/cdpr/web/GameAPIService.java
@@ -3,11 +3,14 @@ package cdpr.web;
 import cdpr.web.resources.Game;
 import cdpr.web.resources.Genre;
 import java.util.HashMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,10 +32,10 @@ public class GameAPIService {
         games.put(id++,newGame);
     }
     
-    @GetMapping("{StringId}")
-    public Game getGameById(@PathVariable String StringId) {
+    @GetMapping("{stringId}")
+    public Game getGameById(@PathVariable String stringId) {
             try{
-                int id = Integer.parseInt(StringId);
+                int id = Integer.parseInt(stringId);
                 if(games.containsKey(id)){
                     return games.get(id);
                 }
@@ -57,6 +60,61 @@ public class GameAPIService {
         games.put(id++, newGame);
         return "Post Ok";
     }
+    @PutMapping("/admin")
+    public String restockQuantity(@RequestParam(name="id") String stringId, @RequestParam(name="q") String stringQuantity){
+        try{
+            int id = Integer.parseInt(stringId);
+            int quantity = Integer.parseInt(stringQuantity);
+            if(quantity < 1){
+                return "Quantity must be grater than 0 to restock";
+            }
+            if(games.containsKey(id)){
+                quantity += games.get(id).getQuantity();
+                games.get(id).setQuantity(quantity);
+                return "Restock succesfully";
+            }
+            return "Such game does not exist";
+        }catch(Exception e){
+            System.out.println("Cannot parse String to int");
+            return null;
+        }
+    }
     @PutMapping
-    public String updateQuantity(@)
+    public String decreaseQuantity(@RequestParam(name="id") String stringId){
+        try{
+            int id = Integer.parseInt(stringId);
+
+            if(games.containsKey(id)){
+                int quantity = games.get(id).getQuantity();
+                if(quantity > 0) {
+                    games.get(id).setQuantity(quantity-1);
+                    return "Quantity Decrased";
+                }
+                return "Product not in stock";
+            }
+            return "Such game does not exist";
+        }catch(Exception e){
+            System.out.println("Cannot parse String to int");
+            return null;
+        }
+    }
+    
+    
+    
+    @DeleteMapping("admin/{stringId}")
+    public String deleteGame(@PathVariable String stringId){
+        try {
+            int id = Integer.parseInt(stringId);
+            if(games.containsKey(id)){
+                games.remove(id);
+                return "Game deleted";
+            }
+            return "Such game does not exist";
+        }catch(Exception e){
+            System.out.println("Cannot parse String to int");
+            return null;
+        }
+    }
+            
+            
 }
