2ce6729890f174cf15e668f77e0ea32b87b8a471
Jan Mich <jaanmiich@gmail.com>
2023-04-25 15:45:06 +0800

    Comments in Repository Classes

--- src/main/java/cdpr/web/repository/GameRepository.java
+++ src/main/java/cdpr/web/repository/GameRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Interface of JpaRepository using Game and Integer to make repository.
+ * Interface of JpaRepository using Game and Integer to make repository. Id of
+ * Game is used as primary key.
  *
  * @author Jan Michalec
  */
 public interface GameRepository extends JpaRepository<Game, Integer> {
+
     List<Game> findAllByName(String name);
 }
--- src/main/java/cdpr/web/repository/UserRepository.java
+++ src/main/java/cdpr/web/repository/UserRepository.java
@@ -4,6 +4,8 @@ import cdpr.web.resources.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
+ * Interface of JpaRepository using User and String to make repository. Login of
+ * User is used as primary key.
  *
  * @author Jan Michalec
  */
